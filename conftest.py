d3ee894f0a866f15f5280680755c2aed3d57fac2
avitiwari-sudo <64724156+avitiwari-sudo@users.noreply.github.com>
2026-06-06 22:04:24 +1000

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -21,9 +21,9 @@ def testData():
 def browserInstance(playwright, request):
     browser_name = request.config.getoption("browser_name")
     if browser_name == "chrome":
-        browser = playwright.chromium.launch(headless=False)
+        browser = playwright.chromium.launch(headless=True)
     elif browser_name == "firefox":
-        browser = playwright.firefox.launch(headless=False)
+        browser = playwright.firefox.launch(headless=True)
 
     context = browser.new_context()
     page = context.new_page()
