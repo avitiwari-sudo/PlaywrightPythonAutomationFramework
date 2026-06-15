51a33543a4ccfe4a8ddea04189287cec4170aaeb
avitiwari-sudo <64724156+avitiwari-sudo@users.noreply.github.com>
2026-06-15 18:58:20 +1000

    Update test_getEmployeeTotalCount.py

--- tests/api/test_getEmployeeTotalCount.py
+++ tests/api/test_getEmployeeTotalCount.py
@@ -14,7 +14,7 @@ def test_getEmployeeTotalCount(browserInstance,api_context,testData,logger):
             all_employees_response = employeeList.getEmployees()
             json_data_employees = all_employees_response.json()
             totalEmployeesAPI = int(json_data_employees['meta']['total'])
-            #csv_path = genCSV.generate_employee_csv()
+            csv_path = genCSV.generate_employee_csv()
             totalEmployeesUI = genCSV.totalRecords(csv_path="updated_orangehrm_employees.csv")
             assert totalEmployeesAPI == totalEmployeesUI, "Total Employees are not matching"
             logger.info(f"EmployeeAPI = {totalEmployeesAPI} matching with EmployeeUI= {totalEmployeesUI}")
